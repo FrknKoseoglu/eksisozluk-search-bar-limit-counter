9b133d3d9c1f8c15af2cabfa9053dfa20cb369c9
Furkan Koseoglu <info@furkankoseoglu.com>
2022-08-02 10:59:54 +0300

    Update eksisozluk-search-bar-limit-counter.js

--- eksisozluk-search-bar-limit-counter.js
+++ eksisozluk-search-bar-limit-counter.js
@@ -1,43 +1,44 @@
- ==UserScript==
- @name         Ekşi Sözlük Search Bar Limit Counter
- @namespace    httptampermonkey.net
- @version      0.1
- @description  try to take over the world!
- @author       FrknKoseoglu
- @match        httpseksisozluk.com
- @icon         httpswww.google.coms2faviconssz=64&domain=eksisozluk.com
- @grant        none
- ==UserScript==
-
- @Github httpsgithub.comFrknKoseoglu
-
+// ==UserScript==
+// @name         Ekşi Sözlük Search Bar Limit Counter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Daha iyi bir Ekşi Sözlük Deneyimi!
+// @author       FrknKoseoglu
+// @match        https://eksisozluk.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=eksisozluk.com
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+ 
+// @Github https://github.com/FrknKoseoglu/
+ 
 (function() {
     'use strict';
     const searchInput = document.querySelector('#search-textbox')
     const searchForm = document.querySelector('#search-form')
-    const countsLetter = document.createElement(div);
-    countsLetter.setAttribute(id, count-letter);
+    const countsLetter = document.createElement("div");
+    countsLetter.setAttribute("id", "count-letter");
     searchForm.prepend(countsLetter)
-    countsLetter.style.cssText = positionabsolute;right15%;font-weightbold;
-
+    countsLetter.style.cssText = "position:absolute;right:15%;font-weight:bold;"
+ 
 const count = document.getElementById('count-letter')
-searchInput.onkeyup = (e) = {
+searchInput.onkeyup = (e) => {
   count.innerHTML = (50 - e.target.value.length);
-    if(e.target.value.length  50) {
-        countsLetter.style.cssText += colorred;
+    if(e.target.value.length > 50) {
+        countsLetter.style.cssText += "color:red;"
     }
     else if(e.target.value.length == 50 ){
-    countsLetter.style.cssText += colorgreen;
+    countsLetter.style.cssText += "color:green;"
     }
-    else if(e.target.value.length  40){
-    countsLetter.style.cssText += colororange;
+    else if(e.target.value.length > 40){
+    countsLetter.style.cssText += "color:orange;"
     }
-    else if(e.target.value.length  35){
-    countsLetter.style.cssText += coloryellow;
+    else if(e.target.value.length > 35){
+    countsLetter.style.cssText += "color:yellow;"
     }
     else{
-    countsLetter.style.cssText += colorunset;
+    countsLetter.style.cssText += "color:unset;"
     }
 };
-
+ 
 })();
